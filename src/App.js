ac83be7349f7d882118160eac39a69342d5daa85
Neejor-Personal <neejorchakma@gmail.com>
2022-04-18 13:31:34 +0530

    feat: shhh

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import SpeechToText from "./SpeechToText";
+import io from "socket.io-client";
+import axios from "axios";
 
 function App() {
+  const [playlistUrl, setPlaylistUrl] = useState("");
+  const socket = io("http://localhost:4000/");
+  socket.emit("wow", "puss");
+  socket.on("hey", (data) => {
+    console.log(data);
+  });
+  socket.on("playlistId", (data) => {
+    console.log(data);
+    setPlaylistUrl(`https://open.spotify.com/playlist/${data.playlistId}`);
+  });
+
+  // const [token, setToken] = useState("");
+
   return (
     <div>
+      {/* <div style={{ minWidth: "30vw" }}>
+        {window.location.href.split("?")[0].split("=")[1] == "true"
+          ? "token received"
+          : "Access Token give"}
+      </div> */}
+      {/* <button
+        onClick={() => {
+          axios
+            .get("http://localhost:4000/login")
+            .then((res) => console.log(res));
+        }}
+      >
+        GetAccessToken
+      </button> */}
+
+      {playlistUrl.length > 0 ? <a href={playlistUrl}>Open Playlist!</a> : null}
+      {/* {window.location.href.split("?")[0].split("=")[1] == "true" ? ( */}
       <SpeechToText />
+      {/* ) : null} */}
     </div>
   );
 }
--- src/SpeechToText.js
+++ src/SpeechToText.js
@@ -1,23 +1,52 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
+import axios from "axios";
 
 function SpeechToText() {
   const { transcript, resetTranscript } = useSpeechRecognition();
+  const [submit, setSubmit] = useState(false);
 
   if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
     return null;
   }
 
   return (
-    <div>
-      <button onClick={SpeechRecognition.startListening}>Start</button>
-      <button onClick={SpeechRecognition.stopListening}>Stop</button>
-      <button onClick={resetTranscript}>Reset</button>
-      <p>{transcript}</p>
-    </div>
+    <>
+      <div>
+        <button onClick={SpeechRecognition.startListening}>Start</button>
+        <button
+          onClick={() => {
+            SpeechRecognition.stopListening();
+            setSubmit(true);
+          }}
+        >
+          Stop
+        </button>
+        <button
+          onClick={() => {
+            resetTranscript();
+            setSubmit(false);
+          }}
+        >
+          Reset
+        </button>
+        <p>{transcript}</p>
+      </div>
+      {submit ? (
+        <button
+          onClick={() => {
+            axios.post("http://localhost:4000/getPlaylists", {
+              transcript,
+            });
+          }}
+        >
+          Send transcript?
+        </button>
+      ) : null}
+    </>
   );
 }
 
