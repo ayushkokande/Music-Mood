35167b5b7eed005d24b61669d107e74eebd33904
Ayush Kokande <ayushkokande093@gmail.com>
2022-04-19 10:44:33 +0530

    CSS

--- src/SpeechToText.js
+++ src/SpeechToText.js
@@ -15,37 +15,59 @@ function SpeechToText() {
 
   return (
     <>
-      <div>
-        <button onClick={SpeechRecognition.startListening}>Start</button>
-        <button
-          onClick={() => {
-            SpeechRecognition.stopListening();
-            setSubmit(true);
-          }}
-        >
-          Stop
-        </button>
-        <button
-          onClick={() => {
-            resetTranscript();
-            setSubmit(false);
-          }}
-        >
-          Reset
-        </button>
-        <p>{transcript}</p>
+      <div className="container">
+        <div className="dialog">
+          <h1>Music Mood</h1>
+          <div
+            style={{
+              position: "relative",
+              marginBottom: "10px",
+              display: "flex",
+              justifyContent: "start",
+              width: "40%",
+              gap: "15px",
+            }}
+          >
+            <button onClick={SpeechRecognition.startListening}>Start</button>
+            <button
+              onClick={() => {
+                SpeechRecognition.stopListening();
+                setSubmit(true);
+              }}
+            >
+              Stop
+            </button>
+            <button
+              onClick={() => {
+                resetTranscript();
+                setSubmit(false);
+              }}
+            >
+              Reset
+            </button>
+          </div>
+          <div className="textBox">
+            {transcript.length ? (
+              <p>{transcript}</p>
+            ) : (
+              <span style={{ color: "rgb(143 143 143)" }}>
+                Press start to speak something!
+              </span>
+            )}
+          </div>
+          <button
+            onClick={() => {
+              axios.post("http://localhost:4000/getPlaylists", {
+                transcript,
+              });
+            }}
+          >
+            Send transcript?
+          </button>
+        </div>
+        {/* {submit ? ( */}
+        {/* ) : null} */}
       </div>
-      {submit ? (
-        <button
-          onClick={() => {
-            axios.post("http://localhost:4000/getPlaylists", {
-              transcript,
-            });
-          }}
-        >
-          Send transcript?
-        </button>
-      ) : null}
     </>
   );
 }
